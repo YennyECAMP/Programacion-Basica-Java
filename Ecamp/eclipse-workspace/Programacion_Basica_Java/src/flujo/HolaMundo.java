45c44e631a0a10536f6b1d9cafd36904710e2881
Yenny Chipamo para ECAMP <82519159+YennyECAMP@users.noreply.github.com>
2021-05-02 15:51:57 -0400

    Add files via upload

---
+++ Ecamp/eclipse-workspace/Programacion_Basica_Java/src/flujo/HolaMundo.java
@@ -0,0 +1,20 @@
+package flujo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+ 
+		/* *********************************************************************************************************************
+		Ejercicio 1 - Mi primera aplicación en Java
+		Objetivo: Método para desplegar mensaje de "Hola Mundo" por pantalla
+		Fecha de Creación: 28-04-2021
+		Autor: Yenny Chipamo
+		Curso: M1 - DESARROLLO DE APLICACIONES FULL STACK JAVA TRAINEE - 0001 - PROGRAMACIÓN BÁSICA EN JAVA - [C51754-O34900-M1]
+		************************************************************************************************************************ */
+		
+		
+		System.out.printf("Hola Mundo");
+
+	}
+
+}
